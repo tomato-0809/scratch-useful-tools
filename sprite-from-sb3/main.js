1a77190c7ee19ba5822a77f205b968dff3e5f2c0
tomato-0809 <tomato.2010.0809@gmail.com>
2025-08-05 21:40:50 +0900

    いろいろした

---
+++ sprite-from-sb3/main.js
@@ -0,0 +1,32 @@
+import htmlEscape from "/lib/htmlEscape.js";
+
+const sb3Input = document.getElementById("sb3Input");
+const spriteSelector = document.getElementById("spriteSelector");
+sb3Input.value = "";
+sb3Input.addEventListener("change", function (e) {
+  window.onbeforeunload = e => e.preventDefault();
+  const file = e.target.files[0];
+  if (!file) return;
+  JSZip.loadAsync(file).then(function (zip) {
+    if (!zip.files["project.json"]) {
+      alert("project.jsonが見つからないじゃない！？sb3ファイル以外なんかよこさないでよっ！");
+      return;
+    }
+    zip.files["project.json"].async("string").then(function (jsonStr) {
+      try {
+        const json = JSON.parse(jsonStr);
+        spriteSelector.innerHTML = ""; // 既存の選択肢をクリア
+        json.targets.forEach((element, i) => {
+          const spriteSelectorElement = document.createElement("div");
+          spriteSelectorElement.innerHTML = `<label><input type="radio" name="spriteSelector" data-index="${i}">${htmlEscape(element.name) + (element.isStage ? " (非推奨)" : "")}</label>`;
+          spriteSelector.append(spriteSelectorElement);
+        });
+        spriteSelector.childNodes[1].checked = true;
+      }catch(err){
+        alert("JSONの解析に失敗したんだけど！？変なファイルよこさないでよっ！");
+      }
+    });
+  }).catch(function (err){
+    alert("sb3の展開に失敗したんだけど！？変なファイルよこさないでよっ！");
+  });
+});
