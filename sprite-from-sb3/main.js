c02362ef939fe533d36c1d4a07f22bb2a5e18485
tomato-0809 <tomato.2010.0809@gmail.com>
2025-08-14 16:49:12 +0900

    revoke周りの処理を改善

--- sprite-from-sb3/main.js
+++ sprite-from-sb3/main.js
@@ -6,15 +6,17 @@ const submit = document.querySelector("#submit");
 const download = document.querySelector("#download");
 
 sb3Input.value = "";
-sb3Input.addEventListener("change", function (e) {
+download.addEventListener("click", downloadListener);
+
+sb3Input.addEventListener("change", function(e){
   const file = e.target.files[0];
   if (!file) return;
-  JSZip.loadAsync(file).then(function (zip) {
+  JSZip.loadAsync(file).then(function(zip){
     if (!zip.files["project.json"]) {
       alert("project.jsonが見つからないじゃない！？sb3ファイル以外なんかよこさないでよっ！");
       return;
     }
-    zip.files["project.json"].async("string").then(function (jsonStr) {
+    zip.files["project.json"].async("string").then(function(jsonStr){
       try {
         const json = JSON.parse(jsonStr);
         console.log(json);
@@ -28,33 +30,39 @@ sb3Input.addEventListener("change", function (e) {
         if(firstSprite) firstSprite.checked = true;
         window.onbeforeunload = e => e.preventDefault();
         submit.style.display = "block";
-      }catch(err){
+      }catch(e){
         alert("JSONの解析に失敗したんだけど！？変なファイルよこさないでよっ！");
         spriteSelector.innerHTML = "";
         submit.style.display = "none";
       }
     });
-  }).catch(function (err){
+  }).catch(function(){
     alert("sb3の展開に失敗したんだけど！？変なファイルよこさないでよっ！");
     spriteSelector.innerHTML = "";
     submit.style.display = "none";
   });
 });
 
-submit.addEventListener("click", function () {
+const a = document.createElement("a");
+const downloadListener = function () {
+  a.click();
+}
+
+submit.addEventListener("click", function(){
+  if(a.href) URL.revokeObjectURL(a.href); // 先にrevokeしておく
   const file = sb3Input.files[0];
-  if (!file) {
+  if(!file){
     alert("sb3ファイルが選択されていないじゃない！？勝手に選択解除しないでよっ！"); // sb3InputでEventListenerを設定してるので、どう考えてもこんな状況になる前に更新が行われるはず
     return
   }
     const selectedSprite = spriteSelector.querySelector("input[name='spriteSelector']:checked");
-  if (!selectedSprite) {
+  if(!selectedSprite){
     alert("スプライトが選択されていないじゃない！？");
     return;
   }
   const index = selectedSprite.dataset.index;
-  JSZip.loadAsync(file).then(function (zip) {
-    zip.files["project.json"].async("string").then(async function (jsonStr) {
+  JSZip.loadAsync(file).then(function(zip){
+    zip.files["project.json"].async("string").then(async function(jsonStr){
       const sprite = JSON.parse(jsonStr).targets[index];
       if (sprite.isStage && !confirm("ステージを選択しているようです。Scratchで読み込んだ場合の動作は一切保証されませんが、よろしいですか？")){
         return;
@@ -65,17 +73,12 @@ submit.addEventListener("click", function () {
 
       // ダウンロード
       const sprite3Blob = await sprite3.generateAsync({type: "blob", compression: "DEFLATE"});
-      const a = document.createElement("a");
       a.href = URL.createObjectURL(sprite3Blob);
       a.download = `${sprite.name}.sprite3`;
       download.style.display = "block";
-      const downloadListener = function () {
-        a.click();
-      }
-      download.addEventListener("click", downloadListener);
       download.click();
     });
-  }).catch(function (err){
+  }).catch(function(){
     alert("sb3の展開に失敗したんだけど！？変なファイルよこさないでよっ！");
   });
 });
